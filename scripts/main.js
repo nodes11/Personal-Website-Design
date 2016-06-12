4d8d4f369017a68421615a9260a11b4bfe08c851
nodes11 <desmarks11@gmail.com>
2016-06-12 16:26:36 -0700

    Dropdown menu working.

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(event){
   $(".menu").on("click", function(event){
-    
+    if ($(".dropdown")[0].style.display == "none"){
+      $(".dropdown")[0].style.display = "block";
+    }
+    else{
+      $(".dropdown")[0].style.display = "none";
+    }
   });
+
 });
